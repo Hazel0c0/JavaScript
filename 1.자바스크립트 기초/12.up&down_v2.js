1bcc641c1935919cc5e215598fa901300ac3893b
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-03-16 17:44:11 +0900

    ~반복문

--- 1.자바스크립트 기초/12.up&down_v2.js
+++ 1.자바스크립트 기초/12.up&down_v2.js
@@ -1,22 +1,39 @@
-var random = 45;
-// var random = Math.floor(Math.random() * 49) + 1;
-var num = 5; //횟수
+var random = Math.floor(Math.random() * 49) + 1;
 
-var answer = +prompt('1~50 사이의 숫자를 5번 안에 맞춰보세요!');
+var num = +prompt(`난이도를 설정하세요~~~~~
+[1.상(기회3번)   2.중(기회6번)   3.하(기회10번)]`);
+var chance;
+if (num === 1) {
+  chance = 3;
+} else if (num === 2) {
+  chance = 6;
+} else {
+  chance = 10;
+}
 
-while (num != 0) {
-  if (answer >= 1 && answer <= 50) {
-    --num;
+var answer = +prompt(`1~50 사이의 숫자를 ${chance}번 안에 맞춰보세요!`);
+var max = 50,
+  min = 1;
+
+while (chance != 0) {
+  if (answer >= min && answer <= max) {
+    --chance;
+    if (chance === 0) {
+      alert('기회를 모두 소진했습니다. GAME OVER!');
+      break;
+    }
     if (random < answer) {
-      answer = +prompt(`down!!\n기회가 ${num}번 남았습니다.`);
+      max = answer;
+      answer = +prompt(`down!!\n기회가 ${chance}번 남았습니다.(${min}~${max})`);
     } else if (random > answer) {
-      answer = +prompt(`up!!\n기회가 ${num}번 남았습니다.`);
+      min = answer;
+      answer = +prompt(`up!!\n기회가 ${chance}번 남았습니다.(${min}~${max})`);
     } else {
       alert('정답입니당  $#%$&^^#');
       break;
     }
+
   } else {
     answer = +prompt('1~50사이의 숫자를 입력해주세요ㅜㅜ');
   }
 }
-alert('기회를 모두 소진했습니다. GAME OVER!');
