62e57a6cf8b236b18ee4915679cc8d4c25895814
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-03-21 16:54:15 +0900

    ----

---
+++ 5.ES6/5. 3 문제 .js
@@ -0,0 +1,32 @@
+const userList = [
+  {
+      account: 'abc1234',
+      userName: '대길이',
+      job: '추노',
+      address: '서울',
+      hobbys: ['수영', '축구', '테니스']
+  },
+  {
+      account: 'banana',
+      userName: '빠나나',
+      job: '과일',
+      address: '서울',
+      hobbys: ['푸드파이팅', '테니스']
+  },
+  {
+      account: 'park1234',
+      userName: '주차왕',
+      job: '발렛파킹',
+      address: '경기',
+      hobbys: ['족구', '축구', '테니스', '영화감상']
+  },
+  {
+      account: 'fire',
+      userName: '불꽃남자카리스마',
+      job: '게이머',
+      address: '서울',
+      hobbys: ['독서', '테니스']
+  },
+];
+
+userList
