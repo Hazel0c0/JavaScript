b0bc0653dcf09ad7ca28b4f39be1ba105341b045
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-03-22 14:22:27 +0900

    dom공부중

--- 5.ES6/0322.js
+++ 5.ES6/0322.js
@@ -1,4 +1,72 @@
-//----------------------------2----------------------
+
+
+
+//-------------객체 구조분해할당 ----------------
+const emp = {
+  empName: '빡빡이',
+  age : 29,
+  hireDate: '2014-01-01',
+  birthDay: '1995-12-31'
+};
+
+// { empName : en } -> 키값 이름을 변경 할 수 있음
+
+
+//------------------------- 배열 구조분해 할당---------------
+
+const userNames = ['김철수', '박영희', '홍길동'];
+
+const [kim, park, hong] = userNames;
+
+// console.log(kim);   //김철수
+// console.log(park);
+// console.log(hong);
+
+const [firstObj]  = [{id:1},{id:2},{id:3}]    // id:1
+  .filter(obj => obj.id >=2);                 //id:2
+
+// console.log(firstObj);
+
+// 번호교환하기
+let first =10 , second =20;
+[second, first] = [first, second] ;
+
+// console.log(first);   //20
+// console.log(second);  //10
+
+//앞에 2개는 변수에 각각 넣고 나머지는 다시 배열로 묶고 싶다
+const numbers=[1,3,5,7,9,11];
+
+//copy
+/*
+const copyNumbers = numbers.slice();
+
+const one = copyNumbers.shift();
+const three = copyNumbers.shift();
+const other = copyNumbers;
+
+*/
+
+const [one, three, ...other]  = numbers;
+
+// console.log(one);       // 1
+// console.log(three);     // 3
+// console.log(other);     // [ 5, 7, 9, 11 ]
+// console.log(numbers);   // 원본은 변하지 않음
+
+
+//스프레드로 배열 간편복사
+const foods = ['감튀', '징거버거'];
+
+//const copyFoods = foods;  //조소 복사
+const copyFoods = [...foods,'카피하면서','추가가능'];   //실제 데이터 복사
+//유사배열을 배열로 만들때 유용하게 쓰임
+//유사배열은 .slice 안되니까
+// console.log(foods);
+// console.log(copyFoods);
+
+
+//----------------------------클로저활용----------------------
 
 function counterClosure() {
   let count = 0;
@@ -82,7 +150,7 @@ console.log(counter_(c => ++c ));     //1
 console.log(counter_(c => c += 3 ));  //3          //4
 console.log(counter_(c => c **2 ));   //Infinity   //16
 
-//-----------------------1--------------------------
+//-----------------------클로저함수--------------------------
 
 let count = 0; //전역변수라서 다른곳에서 count에 개입할 수 있다
 
